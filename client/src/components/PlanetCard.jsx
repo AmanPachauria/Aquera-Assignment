9a015bd6a65a7031071050645c05c02d5d3aa697
aman <amanpachauria@gmail.com>
2024-02-24 14:34:29 +0530

    add no resident condition

--- client/src/components/PlanetCard.jsx
+++ client/src/components/PlanetCard.jsx
@@ -28,6 +28,7 @@ export default function PlanetCard({ planet }) {
               return data;
             })
           );
+          console.log(details);
           setResidentDetails(details);
         } catch (error) {
           console.error("Error fetching resident:", error);
@@ -83,17 +84,14 @@ export default function PlanetCard({ planet }) {
                 <h2 className="text-2xl font-semibold mb-4">
                   Residents Details
                 </h2>
-                {residentDetails && (
+                
+                {residentDetails.length > 0 ? (
                   <div className="max-h-96 overflow-y-scroll no-scrollbar">
-                    {/* {planetData &&
-                      planetData.map((planet) => (
-                        <PlanetCard key={planet.id} planet={planet} />
-                      ))} */}
                     {residentDetails.map((resident, index) => (
                       <ResidentDetails key={index} data={resident} />
                     ))}
                   </div>
-                )}
+                ) : (<div className="text-xl font-semibold mb-4"> No Resident Found </div>)}
                 <button
                   onClick={closeResidents}
                   className="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-md bg-yellow-500 hover:bg-yellow-400"
